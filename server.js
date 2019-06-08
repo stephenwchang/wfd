74c8dd15042ccb24d146150e0e7597b340b541cf
Andrew Bliach <ambliach@gmail.com>
2019-06-08 11:52:15 -0400

    fixes

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
 
-
 // Handlebars
 app.engine(
   "handlebars",
